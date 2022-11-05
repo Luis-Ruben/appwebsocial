f2b3a338c6e84cfa1b409f9fa6cf2b36ead8a5ae
Luis-Ruben <108769437+Luis-Ruben@users.noreply.github.com>
2022-11-04 19:06:10 -0700

    Update kwitter_room.js

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,4 +1,19 @@
 
+var firebaseConfig = {
+      apiKey: "AIzaSyDAa4TDqk60wpaBJ9LJgqeHt0g2T8x51KM",
+      authDomain: "kwitter-9acf9.firebaseapp.com",
+      databaseURL: "https://kwitter-9acf9-default-rtdb.firebaseio.com",
+      projectId: "kwitter-9acf9",
+      storageBucket: "kwitter-9acf9.appspot.com",
+      messagingSenderId: "237900192209",
+      appId: "1:237900192209:web:07f084d63b66e94129a8f0"
+    };
+    
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    user_name = localStorage.getItem("user_name");
+
+
 //AGREGA TUS ENLACES DE FIREBASE AQUÍ
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
@@ -8,4 +23,3 @@ function getData() {firebase.database().ref("/").on('value', function(snapshot) 
       //Finaliza el código
       });});}
 getData();
-
